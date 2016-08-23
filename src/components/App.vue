72f5b4fd1723e5f491171cf4b06429412d932684
Aaron.Lee <auneeyy@gmail.com>
2016-08-23 14:27:35 +0800

    Init

---
+++ src/components/App.vue
@@ -0,0 +1,30 @@
+<style>
+body {
+  font-family: Helvetica, sans-serif;
+  margin: 0;
+}
+
+</style>
+
+<template>
+  <div id="app">
+    <pager-header></pager-header>
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+import PagerHeader from './PagerHeader.vue'
+
+export default {
+  components: {
+    'pager-header':  PagerHeader
+  },
+  data () {
+    return {
+      msg: 'TODO'
+    }
+  }
+}
+</script>
+
---
+++ src/components/PagerHeader.vue
@@ -0,0 +1,23 @@
+<style>
+  header {
+    color: white;
+    font-size: 18px;
+    background: #000;
+    line-height: 44px;
+    text-align: center;
+  }
+</style>
+
+<template>
+  <header>{{title}}</header>
+</template>
+
+<script>
+  export default {
+    data(){
+      return {
+        title: "A simple todo demo build with vue-2.0"
+      }
+    }
+  }
+</script>
